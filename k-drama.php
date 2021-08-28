5240b9721853cf6c5857c7f6cb4885d9a228d419
Igor Lamoia Queiroz <62469164+igorlamoia@users.noreply.github.com>
2021-08-28 17:04:29 -0300

    versao 2

--- k-drama.php
+++ k-drama.php
@@ -6,7 +6,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>K-Vision K-Dramas</title>
-  <link rel="stylesheet" type="text/css" href="./estilo.css">
   <link rel="stylesheet" type="text/css" href="./styles/estilos.css">
 </head>
 

02aa661e9f3dfea996b5b4c0f054f15c5e81766c
igorlamoia <igorlamoia@hotmail.com>
2021-08-29 07:41:12 -0300

    Ajustes

--- login/busca.php
+++ login/busca.php
@@ -18,20 +18,25 @@ $email 			 = $_POST['email'];
 $senha 			 = $_POST['senha'];
 
 // Guardando na variável $sql a string com os comandos pra ser executada
-$sql = "SELECT * from usuarios WHERE SENHA = $senha";
-// Executando a variável sql Tá dando warning
-if (mysqli_num_rows($conn->query($sql)) > 0) {
+$sql = "SELECT * from usuarios WHERE EMAIL = '$email' AND SENHA = '$senha'";
+$result = mysqli_query($conn, $sql);
+// tentei usar mysqli_query ao inves de $conn->query($sql) mas tb n resolveu kkk
+// Executando a variável sql Tá dando warning, tem que mudar essa linha debaixo
+if ($result) {
+	$linhas = mysqli_num_rows($result);
 	// if (Evento de anime && Igor casado com Vitoria) {
 	// Sem evento de anime, sem otaku piranha
 	// }
-	echo  "Usuário Achado na graça do senhor!";
+	if ($linhas) {
+		echo  "Usuário Achado na graça do senhor!";
+	} else {
+		echo "Usuário Não Achado digita sabosta direito";
+	}
 } else if (!$conn->query($sql)) {
 	// if (Evento de anime == false) {
 	// Levar Vitória pra viajar pra Ibitipoca e ficar hospedado em casa com lareira
 	// }
 	echo "Erro: " . $sql . "<br>" . $conn->error;
-} else {
-	echo "Usuário Não Achado digita sabosta direito";
 }
 // Fechando a conexão com o banco
 $conn->close();

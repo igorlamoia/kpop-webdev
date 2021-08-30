f3fd33d0322642de67a019235a00d6b9b2bfbf60
igorlamoia <igorlamoia@hotmail.com>
2021-08-30 20:19:10 -0300

    Ajustando connecta

--- connecta.php
+++ connecta.php
@@ -3,4 +3,4 @@ $servername = "banco";
 $username = "root";
 $password = "12345";
 $database = "kpop";
-?>
+$conn = new mysqli($servername, $username, $password, $database);
--- k-pop/index.php
+++ k-pop/index.php
@@ -1,4 +1,5 @@
 <?php
+include "../connecta.php";
 // Guardando na variável $sql a string com os comandos pra ser executada
 $sql = "SELECT * from videos";
 $result = mysqli_query($conn, $sql);
@@ -22,47 +23,42 @@ exit;
 </head>
 
 <body>
-<header>
+  <header>
     <div class="logotopo">
-        <img src='../imagem/top.jpg' alt="logo do site">
+      <img src='../imagem/top.jpg' alt="logo do site">
     </div>
     <navbar>
-        <div class="menu">
-            <ul>
-                <li><a href=../home>Homes</a></li>
-                <li><a href=../noticias>Noticiais</a></li>
-                <li><a href=../contato>Contato</a></li>
-                <li><a href="../login">Entrar</a></li>
-            </ul>
-        </div>
+      <div class="menu">
+        <ul>
+          <li><a href=../home>Homes</a></li>
+          <li><a href=../noticias>Noticiais</a></li>
+          <li><a href=../contato>Contato</a></li>
+          <li><a href="../login">Entrar</a></li>
+        </ul>
+      </div>
     </navbar>
-</header>
+  </header>
   <br>
   <h2>MVs</h2></br>
-    
+
   <div id="vid">
-  <iframe width="536" height="320" src="https://www.youtube.com/embed/MqzX9JAZ08U" title="YouTube video player"
-    frameborder="15" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-    allowfullscreen></iframe>
-  <iframe width="536" height="320" src="https://www.youtube.com/embed/CuklIb9d3fI" title="YouTube video player"
-    frameborder="15" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-    allowfullscreen></iframe>
-  <iframe width="536" height="320" src="https://www.youtube.com/embed/XA2YEHn-A8Q" title="YouTube video player"
-    frameborder="15" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-    allowfullscreen></iframe>
-   </div>
+    <iframe width="536" height="320" src="https://www.youtube.com/embed/MqzX9JAZ08U" title="YouTube video player" frameborder="15" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="536" height="320" src="https://www.youtube.com/embed/CuklIb9d3fI" title="YouTube video player" frameborder="15" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="536" height="320" src="https://www.youtube.com/embed/XA2YEHn-A8Q" title="YouTube video player" frameborder="15" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </div>
 
 </body>
 <footer>
-    <span>
-        Contatos:
-    </span> 
-        <a href="https://www.instagram.com/lamoiaigor/">Igor Lamoia
-        <img src="../imagem/instal.png" alt="instagram logo"/></a>
-        <a href="https://www.instagram.com/michael_hinoyama/">Michael Andre
-        <img src="../imagem/instal.png" alt="instagram logo"/></a>
-<span>
+  <span>
+    Contatos:
+  </span>
+  <a href="https://www.instagram.com/lamoiaigor/">Igor Lamoia
+    <img src="../imagem/instal.png" alt="instagram logo" /></a>
+  <a href="https://www.instagram.com/michael_hinoyama/">Michael Andre
+    <img src="../imagem/instal.png" alt="instagram logo" /></a>
+  <span>
     &copy 2021
-</span>    
+  </span>
 </footer>
+
 </html>

483923f9a303febebd4b99bbbba536ae7aa0a143
Igor Lamoia Queiroz <62469164+igorlamoia@users.noreply.github.com>
2021-08-30 20:49:12 -0300

    Add: Alertas

--- login/busca.php
+++ login/busca.php
@@ -7,8 +7,10 @@ $conn = new mysqli($servername, $username, $password, $database);
 include "../alerta.php";
 // Checando se foi bem sucedida
 if ($conn->connect_error) {
-
-	die("Conexão Falhou:" . $conn->connect_error);
+			// alerta de login não existente
+			echo "<script type='text/javascript'> swal('Conexão falhou! $email $senha', '$conn->connect_error','error').then((value) => {
+				javascript:window.location='index.html';
+				});;</script>";
 }
 
 // Pegando os dados dos inputs enviados pelo formulário
@@ -17,7 +19,6 @@ $senha 			 = $_POST['senha'];
 
 // Guardando na variável $sql a string com os comandos pra ser executada
 $sql = "SELECT * from usuarios WHERE EMAIL = 'igor@hotmail.com' AND SENHA = '0998'";
-$result = mysqli_query($conn, $sql);
 
 
 // tentei usar mysqli_query ao inves de $conn->query($sql) mas tb n resolveu kkk
@@ -26,15 +27,11 @@ if ($result) {
 	$linhas = mysqli_num_rows($result); //conta as linhas retornadas
 
 	if ($linhas == 1) {
-		echo "<script type='text/javascript'> swal('Usúario Encontrado! $email $senha', 'verifique seu login.','success').then((value) => {
-				javascript:window.location='index.html';
-				});;</script>";
-		//COLOCA O  header('Location: PAGINA.PHP'); AQUI
-	} else {
-		// alerta de login não existente
-		echo "<script type='text/javascript'> swal('Usúario não encontrado Merda! $email $senha', 'verifique seu login.','error').then((value) => {
-		javascript:window.location='index.html';
-	  });;</script>";
+			alerta('Usúario Encontrado!', 'Logando', 'success', 'index.html');
+			//COLOCA O  header('Location: PAGINA.PHP'); AQUI
+		} else {
+			// alerta de login não existente
+			alerta('Usúario não encontrado!', 'verifique seu login.', 'error', 'index.html');
 	}
 } else if (!$conn->query($sql)) {
 

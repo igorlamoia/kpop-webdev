5d6a4c864acd43058fd8ee6c58af6554c262b12a
igorlamoia <igorlamoia@hotmail.com>
2021-08-30 23:11:02 -0300

    Ajustando

--- videos/insere.php
+++ videos/insere.php
@@ -1,7 +1,7 @@
 <?php
 include "../connecta.php";
-
-if ( $conn->connect_error) {
+include "../alerta.php";
+if ($conn->connect_error) {
 	echo "<script type='text/javascript'> swal('Conexão Falhou:', '$conn->connect_error','success').then((value) => {
 		javascript:window.location='../cadastrar';
 			});;</script>";
@@ -10,21 +10,24 @@ if ( $conn->connect_error) {
 $nome 		= $_POST['nome'];
 $url 			= $_POST['url'];
 $banda  	= $_POST['banda'];
-$usuario_insert = $_SESSION['cpf'];
+$usuario_insert = $_SESSION['email'];
 // Guardando na variável $sql a string com os comandos pra ser executada
 $sql = "INSERT INTO videos (NOME, URL, BANDA, USUARIO_INSERT) VALUES";
-$sql .= "('$nome','$url', '$cidade', '$banda', '$usuario_insert')";
+$sql .= "('$nome','$url', '$banda', '$usuario_insert')";
 
 // Executando a variável sql
 if ($conn->query($sql)) {
 	echo "<script type='text/javascript'> swal('Vídeo Cadastrado com sucesso!', 'Obrigado XD','success').then((value) => {
-		javascript:window.location='../login';
+		javascript:window.location='index.php';
 			});;</script>";
 } else {
-	echo "<script type='text/javascript'> swal('$sql', '$conn->error','success').then((value) => {
-		javascript:window.location='../cadastrar';
+	echo "<script type='text/javascript'> swal('Erro no BD', '$conn->error','success').then((value) => {
+		javascript:window.location='index.php';
 			});;</script>";
 }
 // Fechando a conexão com o banco
 $conn->close();
 ?>
+</body>
+
+</html>
--- videos/verifica.php
+++ videos/verifica.php
@@ -7,43 +7,45 @@ require "../connecta.php";
 <!DOCTYPE html>
 
 <html lang="pt-br">
-	<head>
-		<title>Verifica</title>
-		<meta charset="utf-8">
-	</head>
 
-	<form method="POST"	action="insere.php">
+<head>
+	<title>Verifica</title>
+	<meta charset="utf-8">
+</head>
 
-		<body>
+<form method="POST" action="insere.php">
 
-			<?php 
-			$nome 		= $_POST['nome'];
-			$url 			= $_POST['url'];
-			$banda  	= $_POST['banda'];
-			$erro 		= 0;
+	<body>
 
-			//Verifica se o campo nome não está em branco
-			if (empty($nome) || !strstr($nome, ' ')) {
-				$mensagemErro = "Favor digitar o seu nome corretamente.<br>";
-				$erro = 1;
-			}
+		<?php
+		$nome 		= $_POST['nome'];
+		$url 			= $_POST['url'];
+		$banda  	= $_POST['banda'];
+		$erro 		= 0;
 
-			//Verifica se o campo url está preenchido corretamente
-			if (strlen($url)< 8 || !strstr($url, 'https://')) {
-				$mensagemErro = "Favor digitar a url corretamente.<br>";
-				$erro = 1;
-			}
+		//Verifica se o campo nome não está em branco
+		if (empty($nome) || !strstr($nome, ' ')) {
+			$mensagemErro = "Favor digitar o seu nome corretamente.";
+			$erro = 1;
+		}
 
-			//Verifica se não houve erro - neste caso chama a include para inserir os dados
-			if ($erro == 0) {
-				include 'insere.php';
-			} else {
-				//Caso apresente erro, mostra o alert e volta para o index.html
-		
-				echo "<script type='text/javascript'> swal('Usuário não cadastrado', '$MensagemErro','error').then((value) => {
+		//Verifica se o campo url está preenchido corretamente
+		if (strlen($url) < 8 || !strstr($url, 'https://')) {
+			$mensagemErro = "Favor digitar a url corretamente.<br>";
+			$erro = 1;
+		}
+
+		//Verifica se não houve erro - neste caso chama a include para inserir os dados
+		if ($erro == 0) {
+			include 'insere.php';
+		} else {
+			//Caso apresente erro, mostra o alert e volta para o index.html
+
+			echo "<script type='text/javascript'> swal('Usuário não cadastrado', '$mensagemErro','error').then((value) => {
 				 javascript:window.location='index.html';
 					 });;</script>";
-			}
-			?>
-</body>
+		}
+		?>
+	</body>
+
 </html>

92636bf4beb9cf1f1b3db45e9e30fb7c7aaf65e3
Igor Lamoia Queiroz <62469164+igorlamoia@users.noreply.github.com>
2021-08-30 21:04:32 -0300

    Voltando

--- login/busca.php
+++ login/busca.php
@@ -1,24 +1,19 @@
 <?php
 // Configurando as variáveis pra conectar no banco
 include "../connecta.php";
-// Criando a conexao de fato
-$conn = new mysqli($servername, $username, $password, $database);
-
 include "../alerta.php";
 // Checando se foi bem sucedida
 if ($conn->connect_error) {
-			// alerta de login não existente
-			echo "<script type='text/javascript'> swal('Conexão falhou! $email $senha', '$conn->connect_error','error').then((value) => {
-				javascript:window.location='index.html';
-				});;</script>";
-}
 
+	die("Conexão Falhou:" . $conn->connect_error);
+}
 // Pegando os dados dos inputs enviados pelo formulário
 $email 			 = $_POST['email'];
 $senha 			 = $_POST['senha'];
 
 // Guardando na variável $sql a string com os comandos pra ser executada
 $sql = "SELECT * from usuarios WHERE EMAIL = 'igor@hotmail.com' AND SENHA = '0998'";
+$result = mysqli_query($conn, $sql);
 
 
 // tentei usar mysqli_query ao inves de $conn->query($sql) mas tb n resolveu kkk
@@ -27,14 +22,15 @@ if ($result) {
 	$linhas = mysqli_num_rows($result); //conta as linhas retornadas
 
 	if ($linhas == 1) {
-		echo "<script type='text/javascript'> swal('Usúario Encontrado!', 'Logando','success').then((value) => {
-			javascript:window.location='index.html';
-			});;</script>";
-		} else {
-			// alerta de login não existente
-			echo "<script type='text/javascript'> swal('Usúario não encontrado!', 'verifique seu login.','error').then((value) => {
+		echo "<script type='text/javascript'> swal('Usúario Encontrado! $email $senha', 'verifique seu login.','success').then((value) => {
 				javascript:window.location='index.html';
 				});;</script>";
+		//COLOCA O  header('Location: PAGINA.PHP'); AQUI
+	} else {
+		// alerta de login não existente
+		echo "<script type='text/javascript'> swal('Usúario não encontrado Merda! $email $senha', 'verifique seu login.','error').then((value) => {
+		javascript:window.location='index.html';
+	  });;</script>";
 	}
 } else if (!$conn->query($sql)) {
 

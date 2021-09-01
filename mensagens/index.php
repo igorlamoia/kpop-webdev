be7e84fe3000caa9dc11ad356795f3c8438190d4
igorlamoia <igorlamoia@hotmail.com>
2021-08-31 22:45:10 -0300

    Finalizando tela mensagem

--- mensagens/index.php
+++ mensagens/index.php
@@ -25,9 +25,12 @@ if (!$usuario) {
   <link rel="icon" type="image/png" href="../images/VSicone.png" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+  <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css">
   <link rel="stylesheet" href="../styles/reset.css" />
   <link rel="stylesheet" href="../styles/estilos.css" />
   <link rel="stylesheet" href="../styles/formulario.css" />
+  <link rel="stylesheet" href="./mensagens.css" />
+
   <link rel="icon" type="image/png" href="../images/VSicone.png" />
 
 </head>
@@ -49,21 +52,29 @@ if (!$usuario) {
       if (!$mensagem['VISUALIZADA']) {
         $mensagens += 1;
     ?>
-        <div>
-          <span>
-            <?php echo $mensagem['NOME']; ?>
-          </span>  
-          <span>
-            <?php echo $mensagem['EMAIL']; ?>
-          </span>
-          <span>
-            mensagem: <?php echo $mensagem['MENSAGEM']; ?>
+        <div class="cartoes">
+          <div class="dados">
+            <span class="nome linear-text">
+              <?php echo $mensagem['NOME']; ?>
+            </span>
+            <span class="email italico">
+              <?php echo $mensagem['EMAIL']; ?>
+            </span>
+          </div>
+          <span class="mensagem">
+            <?php echo $mensagem['MENSAGEM']; ?>
           </span>
           <div class="final-cartao">
-            <?php echo $mensagem['DATAHORA']; ?>
+            <span class="data italico">
+              <?php
+              $data = explode("-", $mensagem['DATAHORA']);
+              $dia = explode(" ", $data[2]);
+              echo "$dia[0]/$data[1]/$data[0]";
+              ?>
+            </span>
             <div class="icones">
-              <a href="./busca.php?id=<?php echo $mensagem["CODIGO"] . "&acao=1" ?>"><i class="fal fa-comment-alt-times"></i>Marcar como Lida</a>
-              <a href="./busca.php?id=<?php echo $mensagem["CODIGO"] . "&acao=2" ?>"><i class="far fa-envelope"></i>Excluir Mensagem</a>
+              <a href="./busca.php?id=<?php echo $mensagem["CODIGO"] . "&acao=1" ?>"><i class="fas fa-archive"></i></a>
+              <a href="./busca.php?id=<?php echo $mensagem["CODIGO"] . "&acao=2" ?>"><i class="far fa-trash-alt"></i></i></a>
             </div>
           </div>
         </div>
@@ -75,13 +86,16 @@ if (!$usuario) {
     }
     ?>
   </section>
+  <ul class="quadradinhos"></ul>
   <footer>
-      <span>Contatos:</span> 
-      <a href="https://www.instagram.com/lamoiaigor/" target="_blank" rel="noopener">Igor Lamoia
-      <img src="../imagem/instal.png" alt="instagram logo"/></a>
-      <a href="https://www.instagram.com/michael_hinoyama/" target="_blank" rel="noopener">Michael Andre
-      <img src="../imagem/instal.png" alt="instagram logo"/></a>
-      <span>&copy 2021</span>    
-    </footer>
+    <span>Contatos:</span>
+    <a href="https://www.instagram.com/lamoiaigor/" target="_blank" rel="noopener">Igor Lamoia
+      <img src="../imagem/instal.png" alt="instagram logo" /></a>
+    <a href="https://www.instagram.com/michael_hinoyama/" target="_blank" rel="noopener">Michael Andre
+      <img src="../imagem/instal.png" alt="instagram logo" /></a>
+    <span>&copy 2021</span>
+  </footer>
 </body>
+<script src="../scripts/formulario.js"></script>
+
 </html>
